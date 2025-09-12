7bd9f0c09063d56102b73ffa2c9b80dcb870d614
Gustavo Salesi <151581349+Gustavoksbr@users.noreply.github.com>
2025-09-12 19:45:43 -0300

    Atualizar url pro novo back end

--- src/app/services/config/api.config.ts
+++ src/app/services/config/api.config.ts
@@ -1,6 +1,6 @@
 ﻿export const API_CONFIG = {
-  BASE_URL: 'https://roommaker.onrender.com'
+  BASE_URL: 'https://roommaker-2-7.onrender.com'
 };
 
-//https://roommaker.onrender.com
+//https://roommaker-2-7.onrender.com
 //http://localhost:8080
