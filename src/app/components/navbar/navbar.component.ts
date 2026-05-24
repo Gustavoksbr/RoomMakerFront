b3bb772dc1f1d755bfb18d0eaaa538435a735cf4
Gustavoksbr <gustavosalesi@hotmail.com>
2026-05-24 17:03:25 -0300

    removendo cursor: not-allowed do botao 'suas salas'

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
 import {AuthService} from '../../services/auth/auth.service';
+import {AuthModalService} from '../../services/auth/auth-modal.service';
 import {PaginaAtual} from './PaginaAtual';
 import {NgClass} from '@angular/common';
 @Component({
@@ -14,12 +15,20 @@ import {NgClass} from '@angular/common';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private authModalService: AuthModalService) {
   }
 @Input() paginaAtual: PaginaAtual = PaginaAtual.SALAS;
 
   get suasSalasDesabilitado(): boolean {
     return !this.authService.isLoggedIn();
+  }
+
+  navegarParaSuasSalas(): void {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/suas-salas']);
+    } else {
+      this.authModalService.openLogin();
+    }
   }
 
   protected readonly PaginaAtual = PaginaAtual;
